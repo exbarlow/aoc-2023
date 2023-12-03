8fc91e53f52525dd32041330a1d53682500d91d5
Eli Barlow <exbarlow@gmail.com>
2023-12-02 22:30:50 -0600

    day 2 completed

---
+++ day2/t2.cpp
@@ -0,0 +1,65 @@
+#include <fstream>
+#include <iostream>
+using std::cin, std::cout;
+
+int ParseNumColor(std::string& line, size_t start_idx, std::string color) {
+    size_t stop_idx = line.find_first_of(';',start_idx);
+    if (stop_idx == std::string::npos) {
+        stop_idx = line.size()-1;
+    }
+    size_t color_idx = line.find(color, start_idx);
+    // if color not in the showing, return 0
+    if (color_idx == std::string::npos || color_idx > stop_idx) {
+        return 0;
+    }
+
+    size_t num_start = color_idx - 1;
+    while (std::isdigit(line[num_start-1])) {
+        num_start--;
+    }
+    int ans = std::stoi(line.substr(num_start, color_idx-num_start-1));
+    return ans;
+}
+
+int main() {
+    std::string filename;
+    cout << "Enter input file name: \n";
+    cin >> filename;
+    std::ifstream input(filename);
+
+    if (!input.is_open()) {
+        cout << "File was not opened correctly\n";
+        return -1;
+    }
+    std::string line;
+    uint32_t sum = 0;
+
+    while (std::getline(input, line)) {
+        size_t start_idx = 0;
+
+        int red_max = 0;
+        int blue_max = 0;
+        int green_max = 0;
+
+        while (true) {
+            int red = ParseNumColor(line, start_idx, "red");
+            int blue = ParseNumColor(line, start_idx, "blue");
+            int green = ParseNumColor(line, start_idx, "green");
+
+            red_max = std::max(red, red_max);
+            blue_max = std::max(blue, blue_max);
+            green_max = std::max(green, green_max);
+
+            size_t next_start = line.find_first_of(';',start_idx);
+
+            if (next_start != std::string::npos) {
+                start_idx = next_start + 1;
+            } else {
+                break;
+            }
+        }
+        sum += red_max * blue_max * green_max;
+    }
+
+    cout << "sum: " << sum << "\n";
+}
